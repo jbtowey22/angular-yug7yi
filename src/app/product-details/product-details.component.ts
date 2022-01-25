c676b1dc5361c5423f17d5f2d08862df4e6417cb
jbtowey22 <jbtowey@outlook.com>
2022-01-25 08:06:36 -0700

    Completed "Adding navigation" part of the "Try it" example

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  product: Product | undefined;
 
-  ngOnInit(): void {
+  //ActivatedRoute is specific to each component that the Angular Router loads. ActivatedRoute contains information about the route and the route's parameters.
+
+  //By injecting ActivatedRoute, you are configuring the component to use a service. The Managing Data step covers services in more detail.
+
+  constructor(private route: ActivatedRoute) { }
+
+  //In the ngOnInit() method, extract the productId from the route parameters and find the corresponding product in the products array.
+
+  ngOnInit() {
+    // First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+  
+    // Find the product that correspond with the id provided in route.
+    this.product = products.find(product => product.id === productIdFromRoute);
+
+    //The route parameters correspond to the path variables you define in the route. To access the route parameters, we use route.snapshot, which is the ActivatedRouteSnapshot that contains information about the active route at that particular moment in time. The URL that matches the route provides the productId . Angular uses the productId to display the details for each unique product.
+
   }
 
 }
