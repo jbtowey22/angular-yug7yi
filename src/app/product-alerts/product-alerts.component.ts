2d97d54cc58580e81068499991a817b363aa2f47
jbtowey22 <jbtowey@outlook.com>
2022-01-25 07:09:14 -0700

    Completion of the "Getting Started" section of the "Try It" app

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -8,13 +8,13 @@ import { Product } from '../products';
 })
 export class ProductAlertsComponent implements OnInit {
   
-//The product alert component takes a product as input from the product list. With that input, it shows or hides the Notify Me button, based on the price of the product. The Phone XL price is over $700, so the Notify Me button appears on that product.
+//The product alert component takes a product as input from //the product list. With that input, it shows or hides the //Notify Me button, based on the price of the product. The //Phone XL price is over $700, so the Notify Me button //appears on that product.
 
 // The @Input() decorator indicates that the property value //passes in from the component's parent, ProductListComponent
-  //@Input() product!: Product;
-  @Input() product: Product | undefined; //The example doesn't make it clear what this should be
+  @Input() product!: Product;
+  //@Input() product: Product | undefined; //The example doesn't make it clear what this should be
 
-//Configuring ProductAlertsComponent with an @Output() allows the ProductAlertsComponent to emit an event when the value of the notify property changes.
+//Configuring ProductAlertsComponent with an @Output() allows //the ProductAlertsComponent to emit an event when the value //of the notify property changes.
   @Output() notify = new EventEmitter();
   
   constructor() {}
