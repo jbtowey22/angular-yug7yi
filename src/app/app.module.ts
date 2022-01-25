b7a60d904f4e6a4e808f37c13968fdd7c501dcbd
jbtowey22 <jbtowey@outlook.com>
2022-01-25 08:11:33 -0700

    Cosmetic changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,8 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
 
-//The generator automatically added the ProductAlertsComponent to the AppModule to make it available to other components in the application.
+import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 
